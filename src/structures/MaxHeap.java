0f01bbcdc611fdfd6bfc60a3c3741fc8035ee6ce
ahmadammarjr <110498902+KamenYaiba@users.noreply.github.com>
2024-10-23 19:03:13 +0300

    Create MaxHeap.java

---
+++ src/structures/MaxHeap.java
@@ -0,0 +1,22 @@
+package structures;
+
+public class MaxHeap {
+    class Transaction{
+        double amount;
+        double number;
+    }
+    
+    public Transaction getMax(){
+        return null;
+    }
+    
+    public Transaction removeMax(){
+        return null;
+    }
+    
+    public boolean isEmpty(){
+        return false; //
+    }
+    
+        
+}
