d2864a5520de67989e10ed6b64d0dda38fd3ee62
ahmadammarjr <110498902+KamenYaiba@users.noreply.github.com>
2024-10-23 20:14:12 +0300

    Create Transaction.java

---
+++ src/structures/Transaction.java
@@ -0,0 +1,12 @@
+package structures;
+
+
+public class Transaction{
+    double amount;
+    static double number = 0;
+    
+    @Override
+    public String toString(){
+        return null;
+    }
+}
