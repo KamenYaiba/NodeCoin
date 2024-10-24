a621922ab69fd3ea40f2cb56f638d40a2878d0ab
ahmadammarjr <110498902+KamenYaiba@users.noreply.github.com>
2024-10-24 11:08:27 +0300

    Modify class to fit in hr version

--- src/nodecoin/UserInterface.java
+++ src/nodecoin/UserInterface.java
@@ -6,24 +6,31 @@ import structures.*;
 
 public class UserInterface{
 
-    static final int INSERT = 1;
-    static final int GET_MAX = 2;
-    static final int REMOVE_MAX = 3;
-    static final int GET_ALL = 4;
+    final int INSERT = 1;
+    final int GET_MAX = 2;
+    final int REMOVE_MAX = 3;
+    final int GET_ALL = 4;
     
-    static NodeCoin nodeCoin;
+    NodeCoin nodeCoin;
     
-    public static void main(String args[]) {
-        
+    
+    public UserInterface(){
         nodeCoin = new NodeCoin();
-        Scanner input = new Scanner(System.in);
-        
-        while(input.hasNextLine())
-            parse(input.nextLine());
     }
     
     
-    private static void parse(String line){
+    public void run(){
+        Scanner input = new Scanner(System.in);
+        
+        String response;
+        while(input.hasNextLine()){
+            response = parse(input.nextLine());
+            System.out.print(response == null? "": response + "\n");
+        }
+    }
+    
+    
+    private String parse(String line){
         
         Scanner reader = new Scanner(line);
         int operation = reader.nextInt();
@@ -31,22 +38,27 @@ public class UserInterface{
         //optional
         double amount = reader.hasNextDouble()? reader.nextDouble(): -1;
         
+        String output = null;
+        
         switch(operation){
             case INSERT:
                 nodeCoin.insert(date, amount);
                 break;
             case GET_MAX:
                 Transaction t = nodeCoin.getMax(date);
-                System.out.println(t == null? -1: t);
+                output = (t == null? "-1": t.toString());
                 break;
             case REMOVE_MAX:
                 nodeCoin.removeMax(date);
                 break;
             case GET_ALL:
                 String s = nodeCoin.getAll(date);
-                System.out.println(s == null? -1: s);
+                output = (s == null? "-1": s);
+                break;
             default:
-                System.out.println(-1);
+                output = "-1";
         }
+        
+        return output;
     }  
 }
